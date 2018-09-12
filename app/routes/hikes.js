0528bc8fa9c1fc5e887b743fb422ae3c1ee80219
Naeim Zarei <naeim.zarei@gmail.com>
2018-09-12 19:39:18 -0400

    update sample route

--- app/routes/hikes.js
+++ app/routes/hikes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/hikes', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.render('hikes', { title: 'My Hiking Blog' });
     next();
 });
