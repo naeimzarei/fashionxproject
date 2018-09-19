c838fe73a2a4a22ab48c35045e89999ddf82033b
Zachary Burk <zachburk7@gmail.com>
2018-09-19 11:04:39 -0400

    Remove setting port for server

--- app/app.js
+++ app/app.js
@@ -40,9 +40,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log('Server running port: ' + port);
-});
-
 module.exports = app;
