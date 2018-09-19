94798e5668bdb7b57adb4cbf881fb8985c837b04
Zachary Burk <zachburk7@gmail.com>
2018-09-19 10:51:01 -0400

    Add landing page with two buttons

--- app/app.js
+++ app/app.js
@@ -40,4 +40,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log('Server running port: ' + port);
+});
+
 module.exports = app;
