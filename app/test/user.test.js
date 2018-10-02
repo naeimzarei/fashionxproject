2f16521a7d7477ebe49c2d3e07d50815e945b7c3
Zachary Burk <zachburk7@gmail.com>
2018-10-02 16:12:31 -0400

    Clean-up test

--- app/test/user.test.js
+++ app/test/user.test.js
@@ -14,11 +14,8 @@ describe('GET /users', () => {
   test('GET /users should return an array of users', async () => {
     moxios.wait(() => {
       const request = moxios.requests.mostRecent()
-      // console.log(request);
-      request.respondWith({ status: 200, response: 'respond with a resource' }) //mocked response
     })
     const result = await request(app).get('/users');
-    // console.log(result)
     expect(Array.isArray(result.body)).toBe(true);
   });
 });
