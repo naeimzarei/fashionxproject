72690d5db8812ae8b95d2c5189f568379271fd2f
Zachary Burk <zachburk7@gmail.com>
2018-10-10 10:55:16 -0400

    Update signup-controller.js

--- app/controllers/signup-controller.js
+++ app/controllers/signup-controller.js
@@ -5,7 +5,10 @@ var Profile = require('../models/Profile');
 var profile_controller = require('./profile-controller');
 
 var signup_controller = {};
-
+/**
+Validates profile account creation input
+@param {object} profile - Profile data
+*/
 signup_controller.validate = (profile) => {
     var profile_info = {
         first_name: profile.first_name,
@@ -29,6 +32,10 @@ signup_controller.validate = (profile) => {
     return util.format_errors_object(profile_info, profile);
 };
 
+/**
+Executes profile account creation
+@param {object} profile - Profile data
+*/
 signup_controller.signup = async (profile) => {
     return await profile_controller.push(
         profile.first_name,
