bb4ed08103b0fdf7a027ca4fb7e3c0b0739bf8ee
Zachary Burk <zachburk7@gmail.com>
2018-10-10 10:45:52 -0400

    Update Auth.js

--- app/models/Auth.js
+++ app/models/Auth.js
@@ -1,3 +1,4 @@
+/** Auth - Handles creating login information for each account */
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 var config = require('../config/config');
@@ -17,6 +18,11 @@ var authSchema = new mongoose.Schema({
     }
 });
 
+/**
+Create authentication credentials
+@param {string} email - User email address on account
+@param {string} password - Desired password that gets hashed
+*/
 authSchema.statics.push = async (email, password) => {
     var hash = await bcrypt.hash(password, 10);
     var auth = new Auth({
@@ -27,16 +33,31 @@ authSchema.statics.push = async (email, password) => {
     return auth;
 };
 
+/**
+Find authentication credentials
+@param {integer} id - Authentication id
+*/
 authSchema.statics.find = async (id) => {
     var auth = await Auth.findById(id);
     return auth;
 };
 
+/**
+Delete authentication credentials
+@param {integer} id - Authentication id
+*/
 authSchema.statics.remove = async (id) => {
     var auth = await Auth.findByIdAndRemove(id);
     return auth;
 };
 
+
+/**
+Update authentication credentials
+@param {integer} id - Authentication id
+@param {string} email - Account email
+@param {string} password - New desired password
+*/
 authSchema.statics.update = async (id, email, password) => {
     var hash = await bcrypt.hash(password, 10);
     var auth = await Auth.findByIdAndUpdate(id, {
