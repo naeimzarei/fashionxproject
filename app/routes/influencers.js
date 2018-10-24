c33b9a165d04198ea55e492304cc6259f9133147
Yashar Asgari <yasharasgari97@gmail.com>
2018-10-24 11:01:55 -0400

    added route to login or profile page and error handling

--- app/routes/influencers.js
+++ app/routes/influencers.js
@@ -29,7 +29,34 @@ router.post('/signup', async (req, res, next) => {
 
 router.post('/login', async(req, res, next) => {
     var result = await credentials_controller.authenticate(req.body);
-    res.json(result);
+    if(result){
+        const posts = [
+            {
+                id: 1,
+                title: 'Title A',
+                date: '12/12/12',
+                img_url: 'https://via.placeholder.com/150x150',
+                clicks: 0
+            },
+            {
+                id: 2,
+                title: 'Title B',
+                date: '12/12/12',
+                img_url: 'https://via.placeholder.com/150x150',
+                clicks: 15
+            },
+            {
+                id: 3,
+                title: 'Title C',
+                date: '12/12/12',
+                img_url: 'https://via.placeholder.com/150x150',
+                clicks: 61
+            }
+        ];
+        res.render('pages/influencers/home', {title: "Home", posts: posts});    
+    }else{
+        res.render('pages/influencers/login', {title: "Login", errors: {'email': 'Incorrect email or password. Please try again or Sign Up'}})
+    }
 });
 
 
