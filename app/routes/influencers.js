e46f976776a4885cf1ea3a10b2f171f666b2746b
Zachary Burk <zachburk7@gmail.com>
2018-10-26 11:26:32 -0400

    Redirect to home with pulling data via email

--- app/routes/influencers.js
+++ app/routes/influencers.js
@@ -22,31 +22,8 @@ router.post('/signup', async (req, res, next) => {
     var errors = await signup_controller.validate(req.body);
 
     if (Object.keys(errors).length === 0 && errors.constructor === Object) {
-        await signup_controller.signup(req.body);
-        const posts = [
-            {
-                id: 1,
-                title: 'Title A',
-                date: '12/12/12',
-                img_url: 'https://via.placeholder.com/150x150',
-                clicks: 0
-            },
-            {
-                id: 2,
-                title: 'Title B',
-                date: '12/12/12',
-                img_url: 'https://via.placeholder.com/150x150',
-                clicks: 15
-            },
-            {
-                id: 3,
-                title: 'Title C',
-                date: '12/12/12',
-                img_url: 'https://via.placeholder.com/150x150',
-                clicks: 61
-            }
-        ];
-        res.render('pages/influencers/home', {title: "Home", posts: posts}); 
+        var posts = await post_controller.findAll(req.body.email);
+        res.render('pages/influencers/home', { title: "Home", posts: posts, moment: moment });
     } else {
         res.render('pages/influencers/signup', { title: 'Sign Up', errors: errors, fields: req.body});
     }
@@ -55,30 +32,8 @@ router.post('/signup', async (req, res, next) => {
 router.post('/login', async(req, res, next) => {
     var result = await credentials_controller.authenticate(req.body);
     if(result){
-        const posts = [
-            {
-                id: 1,
-                title: 'Title A',
-                date: '12/12/12',
-                img_url: 'https://via.placeholder.com/150x150',
-                clicks: 0
-            },
-            {
-                id: 2,
-                title: 'Title B',
-                date: '12/12/12',
-                img_url: 'https://via.placeholder.com/150x150',
-                clicks: 15
-            },
-            {
-                id: 3,
-                title: 'Title C',
-                date: '12/12/12',
-                img_url: 'https://via.placeholder.com/150x150',
-                clicks: 61
-            }
-        ];
-        res.render('pages/influencers/home', {title: "Home", posts: posts});    
+        var posts = await post_controller.findAll(req.body.email);
+        res.render('pages/influencers/home', { title: "Home", posts: posts, moment: moment });
     }else{
         res.render('pages/influencers/login', {title: "Login", errors: {'email': 'Incorrect email or password. Please try again or Sign Up'}})
     }
@@ -89,9 +44,9 @@ router.get('/manual', (req, res, next) => {
     res.render('pages/influencers/manual', {title: "Help"});
 })
 
-router.get('/home', async(req, res, next) => {
-    var posts = await post_controller.findAll('test@example.com');
-    res.render('pages/influencers/home', { title: "Home", posts: posts, moment: moment });
-});
+// router.get('/home', async(req, res, next) => {
+//     var posts = await post_controller.findAll();
+//     res.render('pages/influencers/home', { title: "Home", posts: posts, moment: moment });
+// });
 
 module.exports = router;
