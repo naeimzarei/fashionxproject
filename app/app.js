d0d5a651387471b2f0f7d8df2601b81559cfd4ed
Naeim Zarei <naeim.zarei@gmail.com>
2018-11-01 15:07:58 -0400

    remove unnecessary code

--- app/app.js
+++ app/app.js
@@ -33,7 +33,6 @@ app.use(require('express-session')({
   secret: 'keyboard cat', 
   resave: false, 
   saveUninitialized: false,
-  // cookie: { maxAge: 30 * 24 * 60 * 60 * 1000, secure: false, httpOnly: true } 
   cookie: false
 }));
 
