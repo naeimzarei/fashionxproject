4f9b3222800f4b0d5d59d39f20bfa04ba4652127
Zachary Burk <zachburk7@gmail.com>
2018-11-02 18:06:05 -0400

    Update config

--- config.js
+++ config.js
@@ -5,4 +5,9 @@ config.DATABASE_PASSWORD = '';
 
 config.SESSION_SECRET = '';
 
+config.BUCKET_NAME = 'fashionx-influencer-photos';
+config.REGION ='us-east-2';
+config.ACCESS_KEY_ID = '';
+config.SECRET_ACCESS_KEY = '';
+
 module.exports = config;
